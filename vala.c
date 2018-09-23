6c3f7bdf512ad09115e12597fcade750655c542c
Victor Almeida <38266337+victoralmeida432@users.noreply.github.com>
2018-09-23 18:50:21 -0300

    Update vala.c

--- vala.c
+++ vala.c
@@ -1,50 +1,96 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
-	int t;
-	scanf("%d",&t);
-	while(t--){
-		int n;
-		scanf("%d",&n);
-		int a[n];
-		int i;
-		for(i=0;i<n;i++){
-			scanf("%d",&a[i]);
-		}
-		int height[n];
-		for(i=0;i<n;i++){
-			height[i] = 0;
-		}
-		int flag = 1;
-		for(i=n-1;i>=0;i--){
-			int left = 2*i+1;
-			int right = 2*i+2;
-			int h = 0;
-			if(a[i] != -1){
-				if(left < n && right < n){
-					if(height[left] >= height[right]){
-						height[i] = 1 + height[left];
-					}else{
-						height[i] = 1 + height[right];
-					}
-					if(height[left] - height[right] > 1 || height[right] - height[left] > 1){
-						flag = 0;
-						break;
-					}
-				}else{
-					if(left < n){
-						height[i] = 1 + height[left];
-					}else{
-						height[i] = 1;
-					}
-				}
-			}
-		}
-		if(flag){
-			printf("T\n");
-		}else{
-			printf("F\n");
-		}
-	}
-	return 0;
+int AlturaMin(int a[], int raiz, int numeros){
+  int esq = 2*raiz+1;
+  int dir = 2*raiz+2;
+
+  if(a[raiz] == -1){
+    return 0;
+  }
+
+  if(dir < numeros){
+    if(a[esq] == -1){
+      return 1;
+    }
+
+    if(a[dir] == -1 && a[esq] != 12){
+      return 1;
+    }
+
+    if(a[dir] == -1 && a[esq] == 12){
+      return 2;
+    }
+
+    if(AlturaMin(a, esq, numeros) >= AlturaMin(a, dir, numeros)){
+      return AlturaMin(a, dir, numeros) + 1;
+    }else{
+      return AlturaMin(a, esq, numeros) + 1;
+    }
+  }else{
+    if(esq < numeros){
+        return AlturaMin(a, esq, numeros) + 1;
+      }else{
+        return 1;
+    }
+  }
+}
+
+int AlturaMax(int a[], int root, int numeros)  
+{ 
+  int esq = (2*root) + 1;
+  int dir = (2*root) + 2;
+
+  if (a[root] == -1){
+    return 0;
+  } 
+  else
+  {
+    if(dir < numeros){
+      int laltura = AlturaMax(a, esq, numeros); 
+      int raltura = AlturaMax(a, dir, numeros); 
+    
+      if (laltura > raltura){
+        return(laltura + 1);
+      }else{
+        return(raltura + 1);
+      }
+    }
+    else{
+      if(esq < numeros){
+        int laltura = AlturaMax(a, esq, numeros);
+        return(laltura + 1);
+      }
+      else{
+        return 1;
+      }
+    }
+  }
+} 
+
+int main(void) {
+  int casos, numeros;
+
+  scanf("%d", &casos);
+
+  while(casos--){
+    scanf("%d", &numeros);
+    int arvore[numeros];
+
+    for(int i = 0; i < numeros; i++){
+      scanf("%d", &arvore[i]);
+    }
+
+    int min = AlturaMin(arvore, 0, numeros);
+    int max = AlturaMax(arvore, 0, numeros);
+
+    //printf("min = %d", min);
+    //printf("   max = %d\n", max);
+    if(max-min<=1){
+      printf("T\n");
+    }else{
+      printf("F\n");
+    }
+  }
+  return 0;
 }
